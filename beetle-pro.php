bab6e5eb1a34e416333397e15117cf17d4ec8a18
Thomas W <netzberufler@googlemail.com>
2016-05-03 17:31:02 +0200

    updated language files and changelog

--- beetle-pro.php
+++ beetle-pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://themezee.com/addons/beetle-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the Beetle theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0
+Version: 1.0.1
 Text Domain: beetle-pro
 Domain Path: /languages/
 License: GPL v3
