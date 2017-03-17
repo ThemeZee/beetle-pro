e94ebafed45c6ffbbb6a6788b1fa5d1c65968bb2
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-03-17 12:54:40 +0100

    updated changelog and version number

--- beetle-pro.php
+++ beetle-pro.php
@@ -5,14 +5,14 @@ Plugin URI: http://themezee.com/addons/beetle-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the Beetle theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0.6
+Version: 1.2
 Text Domain: beetle-pro
 Domain Path: /languages/
 License: GPL v3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 Beetle Pro
-Copyright(C) 2016, ThemeZee.com - support@themezee.com
+Copyright(C) 2017, ThemeZee.com - support@themezee.com
 
 */
 
@@ -62,7 +62,7 @@ class Beetle_Pro {
 		define( 'BEETLE_PRO_NAME', 'Beetle Pro' );
 
 		// Define Version Number.
-		define( 'BEETLE_PRO_VERSION', '1.0.6' );
+		define( 'BEETLE_PRO_VERSION', '1.2' );
 
 		// Define Plugin Name.
 		define( 'BEETLE_PRO_PRODUCT_ID', 58393 );
